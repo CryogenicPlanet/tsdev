96ced5f06161e40741468a2c7ec1f26ada1fbd77
Rahul Tarak <cryogenicplanet@gmail.com>
2021-11-21 00:48:37 +0530

    📦 Get .eslintrc at publish time

--- internal/types/consts.go
+++ internal/types/consts.go
@@ -4,7 +4,7 @@ const PACKAGE_NAME = "@cryogenicplanet/tsdev"
 
 const BUNDLE_DTS_PATH = "node_modules/" + PACKAGE_NAME + "/node_modules/dts-bundle/lib/dts-bundle.js"
 
-const ESLINT_CONFIG_PATH = "node_modules/" + PACKAGE_NAME + "/config/.eslintrc"
+const ESLINT_CONFIG_PATH = "node_modules/" + PACKAGE_NAME + "/dist/.eslintrc"
 
 const ESLINT_PATH = "node_modules/" + PACKAGE_NAME + "/node_modules/eslint/bin/eslint.js"
 
